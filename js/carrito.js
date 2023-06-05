f331c5b946c32a6aa2916a3153dfc7d9a6eb17fc
Brian Mrña <brianmrna001@gmail.com>
2023-06-05 13:12:42 -0300

    commit final

--- js/carrito.js
+++ js/carrito.js
@@ -1,17 +1,16 @@
-
 const mostrarCarrito = () => {
     modalContainer.innerHTML ="";
     modalContainer.style.display = "flex";
     const modalHeader= document.createElement("div");
     modalHeader.classList = "modal_header";
     modalHeader.innerHTML =`
-        <h1 class = modal_header_titulo  >Tu Carrito.</h1>
+        <h1 class = "modal_header_titulo">Tu Carrito.</h1>
     `;
     modalContainer.append(modalHeader);
 
     const modalButton= document.createElement("h1");
-    modalButton.classList="modal_header_button ";
-    modalButton.innerHTML="x";
+    modalButton.classList="modal_header_button";
+    modalButton.innerHTML=`<button type="button" class="btn-close btn-close-white" aria-label="Close"></button>`
 
     modalButton.addEventListener("click",()=>{
         modalContainer.style.display= "none";
@@ -19,33 +18,56 @@ const mostrarCarrito = () => {
 
     modalHeader.append(modalButton);
 
-    const listadoCarrito = document.createElement("scroll-container");
+    const listadoCarrito = document.createElement("div");
     listadoCarrito.className = "listado_carrito";
     carrito.forEach((producto)=>{
         const carritoContent= document.createElement("div");
         carritoContent.className ="carrito_content row";
         carritoContent.innerHTML=`
         <img class="img-fluid col-2" src="${producto.img}" alt="">
-        <h2 class="col-4">${producto.nombre}</h2>
-        <p class="col-4">$ ${producto.precio}</p>
+        <h2 class="col-3 lista_nombre">${producto.nombre}</h2>
+        <div class="col-2 divCantidad">
+            <span class="col-4 restar"> - </span>
+            <span class="col-4 cantidad total">${producto.cantidad}</span>
+            <span class="col-4 sumar"> + </span>
+        </div>
+        <p class="col-3 lista_precio">$ ${producto.precio * producto.cantidad}</p>
         `;
         modalContainer.append(carritoContent);
 
-        let eliminar = document.createElement("span");
+        let eliminar = document.createElement("p");
         eliminar.innerHTML = "❌";
-        eliminar.className="eliminar_producto col-2";
+        eliminar.className="eliminar_producto col-2 d-flex";
         carritoContent.append(eliminar);
 
-        eliminar.addEventListener("click",() => {eliminar_producto(producto.id)});
+        eliminar.addEventListener("click",() => {
+            producto.cantidad = 0;
+            eliminar_producto(producto.id);
+            contadorCarrito();
+        });
+        let restarProductos = carritoContent.querySelector(".restar");
+        let sumarProductos = carritoContent.querySelector(".sumar");
+        restarProductos.addEventListener("click", ()=>{
+            if(producto.cantidad > 1){
+                producto.cantidad--;
+                mostrarCarrito();
+                guardarSesion();
+            }
+        });
+        sumarProductos.addEventListener("click", () =>{
+            producto.cantidad++;
+            mostrarCarrito();
+            guardarSesion();
+        });
     });
 
-    total = carrito.reduce((acc,product)=>acc+ product.precio,0);
+    total = carrito.reduce((acc,product)=>acc+ product.precio * product.cantidad,0);
 
     const totalContent= document.createElement("div");
-    totalContent.className ="total_carrito row";
+    totalContent.className ="total_carrito row ";
     totalContent.innerHTML=`
-    <h3 class="col-6"> Total =</h3>
-    <p>$ ${total}</p>
+    <h3 class="col-2 total"> Total =</h3>
+    <h3 class="col-2 total">$ ${total}</h3>
     `;
     modalContainer.append(totalContent);
 };
@@ -54,10 +76,9 @@ verCarrito.addEventListener("click",mostrarCarrito);
 const eliminar_producto = (id) =>{
     const foundId = carrito.find((element) => element.id === id);
 
-    carrito = carrito.filter((productoId) =>{
+    carrito = carrito.filter((productoId) =>{   
         return productoId !== foundId;
     })
-
     mostrarCarrito();
     guardarSesion();
 }
--- js/main.js
+++ js/main.js
@@ -1,162 +1,106 @@
-//DECLARACION DE FUNCIONES
-function ordenarNombre(){
-    productos.sort((a, b) => {
-        const nombreA = a.nombre.toLowerCase();
-        const nombreB = b.nombre.toLowerCase();
-        if (nombreA > nombreB) {
-            return 1;
-        }
-        if (nombreA < nombreB) {
-            return -1;
-        }
-        return 0;
-    });
-}
-function ordenarPrecio(){
-    productos.sort((a,b)=>{return a.precio-b.precio;})
-}
-function calc_descuento (precio,cantidad) { 
-    if(cantidad >= 5){
-        return (precio*cantidad*0.1) ;   
+class Producto{
+    constructor(nombre,precio,cantidad,img){
+        this.nombre = nombre.toUpperCase();
+        this.precio = Number(precio);
+        this.cantidad = parseInt(cantidad);
+        this.img = img.toUpperCase();
     }
-    return 0;
 }
-// function esNum(mensaje){
-//     while(true){
-//         let entrada =Number( prompt(mensaje));
-//         if(!isNaN(entrada) && entrada != null && entrada != "" || entrada == 0){
-//             return entrada;
-//         }
-//         else{
-//             alert("¡Ingrese un numero valido!");
-//             continue;
-//         }
-//     }
-// }
-const sub_total = (precio,cantidad) =>  precio * cantidad;
-function calcular_recargo(recargo){
-    return (precio) => recargo*precio;
-} 
-
-// class Producto{
-//     constructor(nombre,precio,stock){
-//         this.nombre = nombre.toUpperCase();
-//         this.precio = Number(precio);
-//         this.stock = parseInt(stock);
-//     }
-// }
 //DECLARACION DE VARIABLES
 
-
-let calcular_precio_venta = calcular_recargo(1.25);
-let calcular_iva= calcular_recargo(1.21);
 let catalogoFiltrado ="";
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 let total = 0;
 const catalogo = document.getElementById("catalogo");
 const verCarrito= document.getElementById("verCarrito");
 const modalContainer= document.getElementById("modalContainer");
-
-// cambio el código HTML interno
-productos.forEach((producto)=>{
-    let content = document.createElement("div");
-    content.classList="producto d-flex flex-wrap justify-content-center col-3";
-    content.innerHTML =`
-        <img class="img-fluid" src="${producto.img}" alt="">
-        <h2 class="producto_nombre col-12">${producto.nombre}</h2>
-        <p class="precio col-12">$${producto.precio}</p>
+const cantidadCarrito = document.querySelector(".cantidadCarrito");
+const formulario = document.querySelector("#formulario");
+const buscar = document.querySelector("#buscar");
+const buscarNombre = (productos) =>{
+    const texto = formulario.value.toLowerCase();
+    catalogo.innerText = "";
+    for(let product of productos){
+        let nombre = product.nombre.toLowerCase();
+        if(nombre.indexOf(texto) !== -1){
+            contadorCarrito();
+            let content = document.createElement("div");
+            content.classList="producto d-flex flex-wrap justify-content-center col-5 col-lg-3";
+            content.innerHTML =`
+                <img class="img-fluid" src="${product.img}" alt="">
+                <h2 class="producto_nombre col-12">${product.nombre}</h2>
+                <p class="precio col-12">$${product.precio * product.cantidad}</p>
+                
+            `;
+            catalogo.append(content);
         
-    `;
-
-    catalogo.append(content);
-
-    let comprar = document.createElement("button");
-    comprar.className= "agregarCarrito col-12";
-    comprar.innerText = "Agregar al carrito";
-
-    content.append(comprar);
-
-    comprar.addEventListener("click", ()=>{
-        carrito.push({
-            id: producto.id,
-            nombre: producto.nombre,
-            img: producto.img,
-            precio: producto.precio,
-            stock: producto.stock,
-        });
-        guardarSesion();
+            let comprar = document.createElement("button");
+            comprar.className= "agregarCarrito col-12";
+            comprar.innerText = "Agregar al carrito";
         
-    });
-});
+            content.append(comprar);
+        
+            comprar.addEventListener("click", ()=>{
+                alertAddProduct();
+                const productId = carrito.some((foundProduct)=>foundProduct.id === product.id);
+                if(productId){
+                    carrito.map((prod)=>{
+                        if(prod.id === product.id){
+                            prod.cantidad++;
+                        }
+                    });
+                }
+                else{
+                    carrito.push({
+                        id: product.id,
+                        nombre: product.nombre,
+                        img: product.img,
+                        precio: product.precio,
+                        cantidad: product.cantidad,
+                    });
+                }
+                contadorCarrito();
+                guardarSesion();
+            });
+        }
+    }
+    
+}
 
+const contadorCarrito = () =>{
+    if(carrito.length > 0){
+        cantidadCarrito.style.display = "inline-block";
+        cantidadCarrito.innerText = carrito.length;
+    }
+    else{
+        cantidadCarrito.style.display = "none";
+    }
+}
+const alertAddProduct = () =>{
+    Toastify({
+        text: "Añadido al carrito",
+        duration: 3000,
+        newWindow: true,
+        close: true,
+        gravity: "bottom", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+        onClick: function(){
+            mostrarCarrito();
+        } // Callback after click
+      }).showToast();
+}
 const guardarSesion = () => {
     localStorage.setItem("carrito", JSON.stringify(carrito));
 };
-JSON.parse(localStorage.getItem("carrito"));
+const getProductos = async () => {
+    const respuesta= await fetch("../products.json");
+    const productos = await respuesta.json();
+    buscarNombre(productos);
+    formulario.addEventListener("keyup",() => buscarNombre(productos))
+};
+getProductos();
 
-//Inicio del programa
-// while(true){
-//     //Reseteo las variables
-//     total = 0, descuento=0, descuento_total=0;
-//     cant = [];
-//     for (let i = 0; i < productos.length; i++) {
-//         cant.push(0);
-//     }
-//     lista="Lista de productos:";
-//     if(opcion == 1){
-//         while(true){
-//             opcion = menu()-1;
-//             if(opcion == (Number(productos.length) )){
-//                 opcion = esNum("Ordenar busqueda  \n1) Por nombre A-Z.\n2) De Menor a Mayor precio.\n3) Cancelar.")
-//                 if(opcion == 1){
-//                     ordenarNombre();
-//                 }
-//                 else if(opcion == 2){
-//                     ordenarPrecio();
-//                 }
-//                 else{
-//                     alert("Ordenamiento cancelado.")
-//                 }
-//             }
-//             else if(!isNaN(productos[opcion]) && productos[opcion] != null && productos[opcion] != "" || opcion != -1){
-//                 cantidad =Number( esNum("¿Que cantidad desea comprar?\n (Llevando más de 5 productos obtenes una bonificacion del 10%)"));
-//                 precio = Number( sub_total(productos[opcion].precio,cantidad));
-//                 total= total + precio;
-//                 cant[opcion]= cant[opcion] + cantidad;
-//             }
-//            else if(opcion == -1){
-//                 for (let i = 0; i < productos.length; i++) {
-//                     if(cant[i] > 0){
-//                         lista = lista + "\n ("+ cant[i] + ") Baño de crema.";
-//                         descuento = 0;
-//                         descuento = calc_descuento(productos[i].precio,cant[i]);
-//                         total = total - descuento;
-//                         descuento_total += descuento;
-//                     }
-//                 }
-            
-                
-//                 alert(lista+"\n Descuento mayorista: $"+descuento_total+"\n El total de la compra es: $" + total);
-
-//                 opcion = esNum("¡Seleccione una opcion!\n 1)Finalizar compra.\n 2)Cancelar y volver a comprar.\n 3)Cancelar compra y salir.")
-//                 if(opcion == 1 || opcion == 3){
-//                     alert("¡Gracias por utilizar nuestro servicio. Vuelva pronto!");
-//                     break;
-//                 }
-//                 else if(opcion == 2){
-//                     lista="Lista de productos:";
-//                     cant = [];
-//                     for (let i = 0; i < productos.length; i++) {
-//                         cant.push(0);
-//                     }
-//                     total = 0;
-//                     descuento_total = 0;
-//                     continue;
-//                 }
-//             }
-//             else{
-//                 alert("Opcion Invalida");
-//             }
-//         }
-//     }
-// }
